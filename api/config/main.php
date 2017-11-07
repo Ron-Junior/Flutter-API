cd4687e0b9e8f359c1436051011f3aaeb9090cb9
Victor Nogueira <felladrin@gmail.com>
2017-11-07 11:05:42 -0300

    Update `api/config/main.php`

--- api/config/main.php
+++ api/config/main.php
@@ -1,15 +1,14 @@
 <?php
-
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
 );
-
 return [
     'id' => 'app-api',
     'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
     'bootstrap' => ['log'],
     'language' => 'pt-BR',
     'charset' => 'UTF-8',
@@ -38,6 +37,10 @@ return [
             'enableAutoLogin' => false,
             'enableSession' => false,
             'loginUrl' => null
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the api
+            'name' => 'advanced-api',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
